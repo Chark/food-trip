498d0c6dc9c95af536dbbab65d152212e413cae3
Edvinas <edvinas108@gmail.com>
2016-02-14 17:34:41 +0200

    Adding example thymeleaf and webjar based application config

---
+++ src/main/java/com/food/FoodTripApplication.java
@@ -0,0 +1,12 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTripApplication.class, args);
+	}
+}
