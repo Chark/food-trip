b6149bcfc3134c1d2fac12496a59229282b7b4c0
Edvinas Danevicius <edvinas.danevicius@getcujo.com>
2016-03-04 15:30:58 +0200

    Mini example of thymeleaf

--- src/main/java/com/food/web/home/HomeController.java
+++ src/main/java/com/food/web/home/HomeController.java
@@ -1,15 +1,24 @@
 package com.food.web.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String home() {
+    public String home(Model model) {
+        List<String> variables = new ArrayList<>();
+        variables.add("One");
+        variables.add("Two");
+        variables.add("Three!");
+        model.addAttribute("variables", variables);
         return "home/home";
     }
 }
