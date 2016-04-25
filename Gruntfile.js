f171ef0eebb58218cdd3aa43957d4d89a7f19815
arviens <gezzerr@gmail.com>
2016-04-26 01:44:24 +0300

    Grunt configuration, bower configuration.

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.initConfig({
+        uglify: {
+            deps_js: {
+                files: {
+                    'output/dependencies.min.js': [
+                        'src/main/resources/static/vendor/jquery/dist/jquery.js',
+                        'src/main/resources/static/vendor/semantic/dist/semantic.js'
+                    ]
+                }
+            }
+        },
+
+        concat: {
+            css_deps: {
+                files: {
+                    'output/dependencies.css': [
+                        'src/main/resources/static/vendor/semantic/dist/semantic.css'
+                    ]
+                }
+            },
+            css_main: {
+                src: 'src/main/resources/static/css/**/*.css',
+                dest: 'output/main.css'
+            },
+            js_main: {
+                src: 'src/main/resources/static/js/**/*.js',
+                dest: 'output/scripts.js'
+            }
+        },
+
+        watch: {
+            js_main: {
+                files: 'src/main/resources/static/js/**/*.js',
+                tasks: ['concat:js_main'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            },
+            css_main: {
+                files: 'src/main/resources/static/css/**/*.css',
+                tasks: ['concat:css_main'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            }
+        }
+
+
+    });
+    grunt.registerTask('default', 'Do something interesting.', function () {
+        grunt.log.writeln("");
+        grunt.log.writeln("");
+
+        grunt.log.writeln("If you are starting this project for the first time or you want to initialize this projects dependencies and other required files type:");
+        grunt.log.ok(["grunt init"]);
+        grunt.log.writeln("");
+        grunt.log.writeln("If you want to watch files type: ");
+        grunt.log.ok(["grunt watch"]);
+
+        grunt.log.writeln("");
+        grunt.log.writeln("");
+    });
+
+
+    grunt.registerTask('init', ['uglify:deps_js', 'concat:js_main', 'concat:css_deps', 'concat:css_main']);
+    grunt.registerTask('init', ['watch']);
+};
