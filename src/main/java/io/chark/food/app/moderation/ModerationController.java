4f5c5310e1139564a32c3c685b6df3a962506455
Edvinas <edvinas108@gmail.com>
2016-05-14 13:40:47 +0300

    Move moderation controller from domain package

--- src/main/java/io/chark/food/app/moderation/ModerationController.java
+++ src/main/java/io/chark/food/app/moderation/ModerationController.java
@@ -1,4 +1,4 @@
-package io.chark.food.domain.moderation;
+package io.chark.food.app.moderation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
