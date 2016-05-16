b7357ce9e06d072f31d0dc8f42a099d5b1e357e6
Ugnius <ugnius.malukas@gmail.com>
2016-05-16 22:56:45 +0300

    Tough week :// article categories in progress.

--- src/main/java/io/chark/food/app/administrate/article/ArticleCategoryAdministrationService.java
+++ src/main/java/io/chark/food/app/administrate/article/ArticleCategoryAdministrationService.java
@@ -1,7 +1,9 @@
 package io.chark.food.app.administrate.article;
 
+import io.chark.food.app.administrate.audit.AuditService;
 import io.chark.food.domain.article.ArticleCategory;
 import io.chark.food.domain.article.ArticleCategoryRepository;
+import io.chark.food.util.authentication.AuthenticationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,35 @@ import java.util.Optional;
 public class ArticleCategoryAdministrationService {
 
     private final ArticleCategoryRepository categoryRepository;
+    private final AuditService auditService;
 
     @Autowired
-    public ArticleCategoryAdministrationService(ArticleCategoryRepository categoryRepository) {
+    public ArticleCategoryAdministrationService(ArticleCategoryRepository categoryRepository, AuditService auditService) {
         this.categoryRepository = categoryRepository;
+        this.auditService = auditService;
     }
 
     public Optional<ArticleCategory> getCategory(long id) {
         return Optional.of(categoryRepository.findOne(id));
     }
 
+    /**
+     * Get all article categories.
+     *
+     * @return list of article categories.
+     */
     List<ArticleCategory> getCategories() {
         return categoryRepository.findAll();
+    }
+
+    /**
+     * Delete specified article category.
+     *
+     * @param id user account id.
+     */
+    public void delete(long id) {
+        ArticleCategory category = categoryRepository.findOne(id);
+        categoryRepository.delete(category);
+        auditService.info("Deleted Article Category with id: %d", id);
     }
 }
